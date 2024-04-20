722091cf5e05f8d0f95f0cbc7fc4b621668f7028
Matilda-bit <polinaovras@gmail.com>
2024-04-21 01:16:29 +0300

    add results, setInterval instead setTimeout

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,27 +1,28 @@
 import { useRef, forwardRef, useImperativeHandle } from 'react';
 
 const ResultModal = forwardRef(
-    function ResultModal({ result, targetTime }, ref) {
+    function ResultModal({ targetTime, remainingTime, onReset }, ref) {
         const dialog = useRef();
+        const userLost = remainingTime <= 0;
+        const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
         useImperativeHandle(ref, () => {
             return {
                 open() {
-                    //dialog.current.showModal();
-
+                    dialog.current.showModal();
                 }
             };
         });
 
         return (
-            <div ref={ref} className="result-modal">
-                <h2> You {result}</h2>
+            <dialog ref={dialog} className="result-modal">
+                {userLost && <h2> You Lost</h2>}
                 <p> You target time was <strong>{targetTime} seconds.</strong></p>
-                <p> You stopped the timer with <strong>X seconds left.</strong></p>
-                <form method="dialog">
+                <p> You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+                <form method="dialog" onSubmit={onReset}>
                     <button>Close</button>
                 </form>
-            </div>
+            </dialog>
         );
     }
 ) 
